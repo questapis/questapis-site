7ba56ce62640e84a485e86215c083be583948896
Joseph Journet <questapis@gmail.com>
2026-05-09 23:08:51 -0500

    Add sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://questapis.dev",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/apis",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/docs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/games",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/beta",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://questapis.dev/about",
+      lastModified: new Date(),
+    },
+  ];
+}
